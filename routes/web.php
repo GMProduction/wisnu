e3f59b8377da65df948daeb4e2cb00ede6c8491a
pradana <pradanamahendra@gmail.com>
2020-07-23 00:07:03 +0700

    tampilan user

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('login.login');
+});
+
+Route::get('/daftar', function () {
+    return view('login.daftar');
+});
+
+Route::get('/user', function () {
+    return view('user/dashboard');
+});
+
+Route::get('/user/pemohon', function () {
+    return view('user/pemohon');
+});
+
+Route::get('/user/kasus', function () {
+    return view('user/kasus/kasus');
+});
+
+Route::get('/user/tambahkasus', function () {
+    return view('user/kasus/tambahkasus');
+});
+
+Route::get('/user/jadwal', function () {
+    return view('user/jadwal');
 });
